4cda7a9f8bf164ad08469d942cb4a55cd8691b5a
Hai Dang <lampro62147@gmail.com>
2025-11-04 11:16:07 +0700

    nguyento

--- Source.cpp
+++ Source.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
-
+#include <cmath>
 using namespace std;
 int tong(int a, int b);
 int hieu(int a, int b);
 int tich(int a, int b);
 float thuong(int a, int b);
+bool ktsonguyento(int n);
 int tong(int a, int b) {
     return a + b;
 }
@@ -16,9 +17,22 @@ int tich(int a, int b) {
     return a * b;
 }
 
+bool ktsonguyento(int n) {
+    if (n <= 1) {
+        return false; 
+    }
+    for (int i = 2; i <= sqrt(n); i++) {
+        if (n % i == 0) {
+            return false; 
+        }
+    }
+    return true;
+}
+
 int main()
 {
 	int a, b;
+	int n;
 	cout << "Nhap so thu nhat: ";
     cin >> a;
     
@@ -29,6 +43,13 @@ int main()
 	cout << "Tong = " << tong(a, b) << endl;
 	cout << "Hieu = " << hieu(a, b) << endl;
     cout << "Tich = " << tich(a, b) << endl;
+	cout << "Nhap mot so de kiem tra so nguyen to: ";
+    cin >> n;
+    if (ktsonguyento(n)) {
+        cout << n << " la so nguyen to!" << endl;
+    } else {
+        cout << n << " khong phai la so nguyen to!" << endl;
+    }
 	system("pause");
 	return 0;
 }
