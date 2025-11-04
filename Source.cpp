03506313e2c20ea07443a2e981ee1d1ec06a07ed
Hai Dang <lampro62147@gmail.com>
2025-11-04 11:03:10 +0700

    A

--- Source.cpp
+++ Source.cpp
@@ -8,11 +8,27 @@ float thuong(int a, int b);
 int tong(int a, int b) {
     return a + b;
 }
+int hieu(int a, int b) {
+    return a - b;
+}
+
+int tich(int a, int b) {
+    return a * b;
+}
 
 int main()
 {
+	int a, b;
+	cout << "Nhap so thu nhat: ";
+    cin >> a;
+    
+    cout << "Nhap so thu hai: ";
+    cin >> b;
+	
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
-	cout<<"Tong ="<< tong(a, b) << endl;
+	cout << "Tong = " << tong(a, b) << endl;
+	cout << "Hieu = " << hieu(a, b) << endl;
+    cout << "Tich = " << tich(a, b) << endl;
 	system("pause");
 	return 0;
 }
