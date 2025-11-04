7e7c7a3b64a40443efd80d91bfb80880d6e19674
Hai Dang <lampro62147@gmail.com>
2025-11-04 10:56:54 +0700

    -a

--- Source.cpp
+++ Source.cpp
@@ -5,10 +5,14 @@ int tong(int a, int b);
 int hieu(int a, int b);
 int tich(int a, int b);
 float thuong(int a, int b);
+int tong(int a, int b) {
+    return a + b;
+}
+
 int main()
 {
 	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
-	cout<<"Tong =";
+	cout<<"Tong ="<< tong(a, b) << endl;
 	system("pause");
 	return 0;
 }
